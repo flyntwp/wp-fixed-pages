afdff7b22fe9eff9ea57c508e9536cc29216f4bd
Phil Bennett <phil@bleech.de>
2018-04-19 13:28:35 +0200

    Update FixedPages.php

--- lib/FixedPages.php
+++ lib/FixedPages.php
@@ -15,11 +15,11 @@ class FixedPages {
     protected $dbVersionKey = '';
     protected $dbVersion = 0;
 
-    public function __construct($config) {
+    public function __construct($config, $customPostTypeConfig = []) {
         $this->version = $config['version'];
         $this->name    = $config['name'];
 
-        new CustomPostType($this->name);
+        new CustomPostType($this->name, $customPostTypeConfig);
 
         $this->pages       = $this->setPages($config['pages']);
         $this->pagesByPath = $this->mapPagesToPaths($this->pages);
